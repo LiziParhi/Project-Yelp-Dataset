3221e731a1889cbf377bf99c68406285412f7303
Lizilina Parhi <lparhi@scu.edu>
2016-02-24 22:27:25 -0800

    Added files via upload
    You would develop a target application which runs queries on the Yelp data and extracts useful information.The primary users for this application will be potential customers seeking for businesses that match their search criteria. Your application will have a user interface that provides the user the available business categories (main and sub-categories) and the attributes associated with each business category. Using this application the user will search for the businesses from various business categories that have the properties (attributes) the user is looking for. The user can filter the search results by state, city or zipcode. In addition, the user may choose to view only the businesses that are open or closed on certain days of the week. The application should also allow the user to view the reviews provided for each business.
    You will be designing your application a standalone Java application.
    
     Populate your database with the Yelp data. Generate INSERT statements for your

---
+++ DROP_DB_HW3.sql
@@ -0,0 +1,7 @@
+drop table YELP_USER cascade constraints;
+drop table REVIEWS cascade constraints;
+drop table NATIVE_ATTRIBUTE cascade constraints;
+drop table NATIVE_CATEGORY cascade constraints;
+drop table BUSINESS cascade constraints;
+drop table BUSINESS_CATEGORY cascade constraints;
+drop table BUSINESS_SUB_CATEGORY cascade constraints;
